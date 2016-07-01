d46f573f72da87c170464fc2a6b833594d1b70dd
Sophia Shapira <Sophia.Shapira@gmail.com>
2016-07-01 18:55:36 -0400

    Added the Gloria

---
+++ gloria-text.php
@@ -0,0 +1,42 @@
+<p><u>
+Glory be to God on high,
+and on earth, peace to those of good will.
+We praise thee; we bless thee;
+we adore thee;
+we glorify thee.
+We give thee thanks for thy great glory, O Lord God,
+heavenly King,
+God the Father Almighty,
+O Lord Jesus Christ,
+the only begotten Son.
+O Lord God, Lamb of God,
+Son of the Father,
+who takest away the sins
+of the world,
+have mercy on us.
+Who takest away the sins
+of the world,
+receive our prayers.
+Who sittest at the right hand of the
+Father,
+have mercy on us.
+For thou only art holy.
+Thou only art the Lord.
+Thou only, O Jesus Christ,
+together with the Holy Spirit,
+art most high in the glory of God the Father.
+</u></p>
+
+<p><u>Amen.</u></p>
+
+<?php
+$sm->lnk_cite("Source of the Gloria","http://archive.org/details/a550137400unknuoft");
+$sm->cite(" page 29");
+$sm->ct_line(2);
+$sm->cite("MODIFICATIONS");
+$sm->ct_line(2);
+$sm->cite("\"men of good will\" -> \"those of good will\"");
+$sm->ct_link(array());
+//http://ia700200.us.archive.org/1/items/a550137400unknuoft/a550137400unknuoft.pdf
+//Home-page of the document (in multiple formats is):
+?>
